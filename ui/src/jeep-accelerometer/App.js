2fe4d6c7644d67378579ab13a24fd038eacb7e39
Marty Henderson <10hendersonm@gmail.com>
2019-07-07 16:18:08 -0500

    more descriptive output

--- ui/src/jeep-accelerometer/App.js
+++ ui/src/jeep-accelerometer/App.js
@@ -3,19 +3,12 @@ import React, { useEffect, useState } from 'react'
 const App = () => {
   const [data, setData] = useState('No data yet')
   const handleDeviceMotion = e => {
-    console.log(e)
-    setData('data')
+    const dataStr = JSON.stringify(e)
+    setData(dataStr)
   }
   useEffect(() => {
-    // var supportsOrientationChange = !!window.ondeviceorientation
-    // if (supportsOrientationChange) {
-    // setData('Has Event')
     window.addEventListener('deviceorientation', handleDeviceMotion, true)
-    // } else {
-    // setData('no event')
-    // }
     return () => {
-      setData('removing event')
       window.removeEventListener('deviceorientation', handleDeviceMotion, true)
     }
   }, [])
