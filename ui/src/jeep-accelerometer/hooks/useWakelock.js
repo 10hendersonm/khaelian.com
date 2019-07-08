d6ef8bf3d620c8ce7bbd7e6fa82a4e8c4b8788a1
Marty Henderson <10hendersonm@gmail.com>
2019-07-07 19:52:59 -0500

    added the wakelock file

---
+++ ui/src/jeep-accelerometer/hooks/useWakelock.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+
+const useWakelock = () => {
+  const createWakeLockRequest = async () => {
+    const wakeLockObj = await getWakeLockObject()
+    if (wakeLockObj) {
+      const wakeLockRequest = wakeLockObj.createRequest()
+      return wakeLockRequest
+    }
+  }
+
+  const getWakeLockObject = async () => {
+    let wakeLockObj
+    if ('getWakeLock' in navigator) {
+      try {
+        wakeLockObj = await navigator.getWakeLock('screen')
+      } catch (err) {
+        console.log('error creating wakelock', err)
+      }
+    } else {
+      console.log('wake lock not available')
+    }
+    return wakeLockObj
+  }
+
+  useEffect(() => {
+    var wakeLockRequest
+    createWakeLockRequest().then(req => {
+      wakeLockRequest = req
+    })
+    return () => {
+      if (wakeLockRequest) {
+        wakeLockRequest.cancel()
+      }
+    }
+  }, [])
+}
+
+export default useWakelock
