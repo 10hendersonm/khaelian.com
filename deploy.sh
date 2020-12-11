eea39c949a46a1a93c47b2bd8c5b9f2c27e1b704
Marty Henderson <10hendersonm@gmail.com>
2020-12-10 22:26:08 -0600

    chore: Less appending

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "Creating ssh files"
 mkdir -p ~/.ssh
 sudo chmod 700 ~/.ssh
-echo $DEPLOY_SSH_PRIVATE_KEY >> ~/.ssh/id_rsa
+echo $DEPLOY_SSH_PRIVATE_KEY > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
 
 echo "Starting ssh agent"
