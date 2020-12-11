28ed569b113cc596657dc2c09c7537b5b3ce12a0
Marty Henderson <10hendersonm@gmail.com>
2020-12-10 22:40:30 -0600

    chore: New approach

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 echo "Creating ssh files"
-mkdir -p ~/.ssh
+mkdir -p ~/.ssh/
 echo $DEPLOY_SSH_PRIVATE_KEY > ~/.ssh/id_rsa
-echo $DEPLOY_SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
-chmod -R 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+cat >>~/.ssh/config <<END
+HOST webserver
+    HostName $DEPLOY_HOST
+    User $DEPLOY_USER
+    IdentityFile ~/.ssh/id_rsa
+    StrictHostKeyChecking no
+END
 
-echo "Starting ssh agent"
-eval `ssh-agent -s`
-echo "Configuring ssh agent"
-ssh-add ~/.ssh/id_rsa
-
-echo "Attempting ssh connection"
-ssh $DEPLOY_USER@$DEPLOY_HOST "echo foo >> ~/test"
+ssh webserver 'echo foo >> ~/test'
