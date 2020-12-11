e7fd2574e2180b6191e2a5b94221ea3f223be4cb
Marty Henderson <10hendersonm@gmail.com>
2020-12-10 22:07:19 -0600

    chore: adds ssh verbosity

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
+echo "Creating ssh files"
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo $DEPLOY_SSH_PRIVATE_KEY >> ~/.ssh/id_rsa
+
+echo "Configuring ssh agent"
 ssh-agent -s
 ssh-add ~/.ssh/id_rsa
 
+echo "Attempting ssh connection"
 ssh $DEPLOY_USER@$DEPLOY_HOST "echo foo >> ~/test"
