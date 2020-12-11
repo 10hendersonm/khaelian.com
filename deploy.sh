73257fac298a5d709f0290102264112c396b5b30
Marty Henderson <10hendersonm@gmail.com>
2020-12-10 22:06:06 -0600

    chore: Calls ssh-add in a different way

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo $DEPLOY_SSH_PRIVATE_KEY >> ~/.ssh/id_rsa
-ssh-agent $BASH
+ssh-agent -s
 ssh-add ~/.ssh/id_rsa
 
 ssh $DEPLOY_USER@$DEPLOY_HOST "echo foo >> ~/test"
